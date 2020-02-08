4237447af5f67806337726c93b788a1a86304bd3
charli <charli@charli-PC>
2020-02-08 12:32:44 -0700

    #15 => Probando métodos eliminarPrincipio

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -8,16 +8,19 @@ public class Estructuras {
         ListaEnlazada l = new ListaEnlazada();
         l.insertar(8);
         l.insertar(16);
-        l.insertarPrincipio(5);
-        l.insertar(14);
-        l.insertarPrincipio(22);
-        l.insertar(41);
-        l.eliminarUltimo();
-        l.eliminarUltimo();
-        l.eliminarUltimo();
-        l.eliminarUltimo();
+        l.insertarPrincipio(44);
+        l.eliminarPrincipio();
         l.insertarPrincipio(13);
+        l.insertar(41);
+        l.eliminarPrincipio();
+        l.insertarPrincipio(15);
+        l.insertar(56);
+        l.insertar(48);
+        l.eliminarPrincipio();
+        l.eliminarPrincipio();
+        
         System.out.println(l.toString());
+        System.out.println("Hay: " + l.getContador() + " nodos");
     }
     
 }

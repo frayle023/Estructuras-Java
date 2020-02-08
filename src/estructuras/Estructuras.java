40bce28b7e9ea922662e9540f56fa84a4599343c
charli <charli@charli-PC>
2020-02-08 11:29:47 -0700

    #9 => Probando los Métodos toString e insertar

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -6,7 +6,12 @@ public class Estructuras {
 
     public static void main(String[] args) {
         ListaEnlazada l = new ListaEnlazada();
-        System.out.println(l.esVacia());
+        l.insertar(8);
+        l.insertar(16);
+        l.insertar(25);
+        l.insertar(27);
+        l.insertar(9);
+        System.out.println(l.getContador());
     }
     
 }

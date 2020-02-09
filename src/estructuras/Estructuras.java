edf9aecd718304d1dfc127fe53b979296b92c02c
charli <charli@charli-PC>
2020-02-09 13:23:41 -0700

    #16 => Probando métodos eliminarPorDato

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -9,16 +9,25 @@ public class Estructuras {
         l.insertar(8);
         l.insertar(16);
         l.insertarPrincipio(44);
-        l.eliminarPrincipio();
+
         l.insertarPrincipio(13);
         l.insertar(41);
-        l.eliminarPrincipio();
+
         l.insertarPrincipio(15);
         l.insertar(56);
         l.insertar(48);
-        l.eliminarPrincipio();
-        l.eliminarPrincipio();
+
+        l.insertar(16);
+        l.insertarPrincipio(16);
+        l.insertar(16);
+        l.insertar(41);
         
+       
+        l.eliminarPorDato(48);
+        l.eliminarPorDato(8);
+        l.eliminarPorDato(41);
+        l.eliminarPorDato(13);
+   
         System.out.println(l.toString());
         System.out.println("Hay: " + l.getContador() + " nodos");
     }

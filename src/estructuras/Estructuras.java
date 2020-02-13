6db651dc4eb96b657b976b922cab48ac51acfc06
charli <charli@charli-PC>
2020-02-12 17:14:45 -0700

    #21 => Probando Clase: ListaDoble

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -1,29 +1,18 @@
 package estructuras;
 
-import estructurasdatos.ListaEnlazada;
+import estructurasdatos.ListaDoble;
 
 public class Estructuras {
 
     public static void main(String[] args) {
-        ListaEnlazada lista = new ListaEnlazada();
-     
-        lista.insertarFinal("Pedro");
-        lista.insertarFinal("Erika");
-        lista.insertarFinal(4);
-        lista.insertarFinal(6);
-        lista.insertarFinal("GT");
-        lista.insertarPrincipio("Zolia");
-        lista.insertarPrincipio(11);
-        lista.insertarPrincipio(22);
-        lista.insertarPrincipio("A");
-        lista.insertarPrincipio(333);
-        lista.insertarPrincipio(0.5);
-        lista.insertarPrincipio(3.5);
-        lista.insertarFinal(5510);
-        lista.insertarFinal("ped");
-        lista.insertarPrincipio("a");
+        ListaDoble lista = new ListaDoble();
+         lista.insertarPrincipio(12);
+         lista.insertarPrincipio(16);
+         lista.insertarPrincipio(18);
+         lista.insertarFinal(14);
+         lista.insertarFinal(22);
+         lista.insertarPrincipio(5);
         
-        lista.ordenamientoBurbuja();
         
         System.out.println(lista.toString());
         System.out.println("Hay: " + lista.getContador() + " nodos");

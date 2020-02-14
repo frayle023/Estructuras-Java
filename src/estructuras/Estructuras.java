3261ed7b02c9d73e19c50c4d1916a75cdf816dd5
charli <charli@charli-PC>
2020-02-14 13:43:39 -0700

    #31 => Probando Métodos: ordenamientoBurbuja

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -7,19 +7,18 @@ public class Estructuras {
 
     public static void main(String[] args) {
         ListaDoble lista = new ListaDoble();
-         lista.insertarPrincipio(60);
-         lista.insertarPrincipio(50);
-         lista.insertarPrincipio(40);
-         lista.insertarPrincipio(30);
-         lista.insertarPrincipio(20);
-         lista.insertarPrincipio(10);
-         lista.insertarFinal(70);
+         lista.insertarPrincipio("k");
+         lista.insertarPrincipio("j");
+         lista.insertarPrincipio("a");
+         lista.insertarPrincipio("d");
+         lista.insertarPrincipio("z");
+         lista.insertarPrincipio("b");
+         lista.insertarFinal("i");
       
+         lista.ordenamientoBurbuja();
        
         
          
-        
-        lista.reversa();
         System.out.println(lista.toString());
         System.out.println("Hay: " + lista.getContador() + " nodos");
         

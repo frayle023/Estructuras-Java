fffb18cf80ec7249adde9453aa66e4710f2581e2
charli <charli@charli-PC>
2020-02-15 01:09:04 -0700

    #33 => (Pendiente) Modificar Método: insertarFinal

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -8,7 +8,9 @@ public class Estructuras {
     public static void main(String[] args) {
         ListaCircular lista = new ListaCircular();
         
-     
+        lista.insertarFinal(10);
+      
+        lista.insertarFinal(55); 
         
        System.out.println(lista.toString());
        System.out.println("Hay: " + lista.getContador() + " nodos");
--- src/estructurasdatos/ListaCircular.java
+++ src/estructurasdatos/ListaCircular.java
@@ -20,4 +20,36 @@ public class ListaCircular {
         return this.num_nodos;
     }
     
+    public boolean esVacia(){
+        return this.primero == null && this.ultimo == null && this.num_nodos == 0;
+    }
+    
+    public void insertarFinal(Object dato){
+        Nodo nuevo = new Nodo(dato);
+        if(this.esVacia()){
+            nuevo.setSiguiente(nuevo);
+            primero = ultimo = nuevo;
+        }else{
+            Nodo temporal = ultimo;
+            nuevo.setSiguiente(temporal);
+            temporal.setSiguiente(nuevo);
+            ultimo = nuevo;
+        }
+        this.num_nodos++;
+    }
+    
+    
+    @Override
+    public String toString(){
+        if(this.esVacia()) return "Lista Vacia";
+        String cadena = "";
+        Nodo temporal = primero;
+        while(temporal != ultimo){
+            cadena += temporal.toString();
+            temporal = temporal.getSiguiente();
+        }
+        return cadena + temporal.toString();
+    }
+    
+    
 }

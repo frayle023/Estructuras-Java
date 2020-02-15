9e00074a0c6920116ba2899aa8daeef4bb4dbd06
charli <charli@charli-PC>
2020-02-15 00:39:13 -0700

    #32 => Reestructurando Clase ListaCirular

--- src/estructuras/Estructuras.java
+++ src/estructuras/Estructuras.java
@@ -8,19 +8,10 @@ public class Estructuras {
     public static void main(String[] args) {
         ListaCircular lista = new ListaCircular();
         
-        lista.insertarFinal(11);
-        lista.insertarPrincipio(55);
-        lista.insertarFinal(44);
-        lista.insertarPrincipio(66);
+     
         
-        lista.eliminarFinal();
-        lista.eliminarFinal();
-        lista.eliminarFinal();
-        lista.eliminarFinal();
-        lista.eliminarFinal();
-        
-        System.out.println(lista.toString());
-        System.out.println("Hay: " + lista.getContador() + " nodos");
+       System.out.println(lista.toString());
+       System.out.println("Hay: " + lista.getContador() + " nodos");
         
     }
     
--- src/estructurasdatos/ListaCircular.java
+++ src/estructurasdatos/ListaCircular.java
@@ -14,99 +14,10 @@ public class ListaCircular {
     private Nodo ultimo;
     private int num_nodos;
     
+    public ListaCircular(){}
+    
     public int getContador(){
-        return num_nodos;
-    }
-    
-    public boolean esVacia(){
-        return num_nodos == 0;
-    }
-    
-    public void vaciar(){
-        this.num_nodos = 0;
-        primero = null;
-        ultimo = null;
-    }
-    
-    public void insertarPrincipio(Object dato){
-        Nodo nuevo = new Nodo(dato);
-        if(this.esVacia()){
-            primero = nuevo;
-            ultimo = nuevo;
-            primero.setSiguiente(ultimo);
-            ultimo.setSiguiente(primero);
-        }else if(primero == ultimo){
-            primero = nuevo;
-            primero.setSiguiente(ultimo);
-            ultimo.setSiguiente(primero);
-        }else{
-            nuevo.setSiguiente(primero);
-            primero = nuevo;
-            ultimo.setSiguiente(primero);
-        }
-        this.num_nodos++;
-    }
-    
-    public void insertarFinal(Object dato){
-        Nodo nuevo = new Nodo(dato);
-        if(this.esVacia()){
-            primero = nuevo;
-            ultimo = nuevo;
-            primero.setSiguiente(ultimo);
-            ultimo.setSiguiente(primero);
-        }else if(primero == ultimo){
-            ultimo = nuevo;
-            primero.setSiguiente(nuevo);
-            ultimo.setSiguiente(primero);
-        }else{
-            ultimo.setSiguiente(nuevo);
-            ultimo = nuevo;
-            ultimo.setSiguiente(primero);
-        }
-        this.num_nodos++;
-    }
-    
-    public void eliminarPrincipio(){
-        if(!this.esVacia()){
-            if(primero == ultimo){
-                this.vaciar();
-            }else{
-                primero = primero.getSiguiente();
-                ultimo.setSiguiente(primero);
-                this.num_nodos--;
-            }
-        }
-    }
-    
-    public void eliminarFinal(){
-        if(!this.esVacia()){
-            if(primero == ultimo){
-                this.vaciar();
-            }else{
-                Nodo temporal = primero;
-                while(temporal.getSiguiente() != ultimo){
-                    temporal = temporal.getSiguiente();
-                }
-                ultimo = temporal;
-                ultimo.setSiguiente(primero);
-                this.num_nodos--;
-            }
-        }
-    }
-    
-    @Override
-    public String toString(){
-        if(this.esVacia()) return "Lista Vacia";
-        String cadena = "";
-        
-        System.out.println(primero.getDato() + " - " + ultimo.getDato());
-        
-        Nodo temporal = primero;
-        for(int i = 0; i < num_nodos; i++){
-            cadena += temporal.toString();
-            temporal = temporal.getSiguiente();
-        }
-        return cadena;
+        return this.num_nodos;
     }
     
 }
